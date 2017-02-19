05f81aa8f19ae00c7e1b87d198ec573aeb7b3deb
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2017-02-19 14:17:45 +0100

    Build and link Daniel Lemire's implementation

---
+++ build.rs
@@ -0,0 +1,27 @@
+use std::process::Command;
+
+fn run(command: &str, args: &[&str]) {
+    let status = {
+        let msg = format!("Failed to run {}. \
+                           Please make sure it is installed", command);
+        Command::new(command)
+        .args(args)
+        .status()
+        .expect(&msg)
+    };
+    if !status.success() {
+        panic!("{} exited with an error.", command);
+    }
+}
+
+fn main() {
+    std::env::set_current_dir("lemire").unwrap();
+    // Rust requires position-independent code for any static library.
+    run("gcc", &["-std=c99", "-O3", "-march=native", "-fPIC", "-o", "newlines.o", "newlines.c"]);
+    run("ar", &["rcs", "newlines.a", "newlines.o"]);
+    //^ For now we are always building the static library. Ideally this should not be done if it
+    //  was already built.
+
+    println!("cargo:rustc-link-search=native=lemire");
+    println!("cargo:rustc-link-lib=static=newlines");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
+extern "C" {
+    fn basiccount(buffer: *const u8, size: usize) -> usize;
+    fn memchrcount(buffer: *const u8, size: usize) -> usize;
+    fn swarcount(buffer: *const u8, size: usize) -> usize;
+    fn avxcount(buffer: *const u8, size: usize) -> usize;
+    fn avxcountuu(buffer: *const u8, size: usize) -> usize;
+    fn avxcountu(buffer: *const u8, size: usize) -> usize;
+}
+
+pub fn newlinecount_basic(buffer: &[u8]) -> usize {
+    unsafe { basiccount(buffer.as_ptr(), buffer.len()) }
+}
+
+pub fn newlinecount_memchr(buffer: &[u8]) -> usize {
+    unsafe { memchrcount(buffer.as_ptr(), buffer.len()) }
+}
+
+pub fn newlinecount_swar(buffer: &[u8]) -> usize {
+    unsafe { swarcount(buffer.as_ptr(), buffer.len()) }
+}
+
+pub fn newlinecount_avx(buffer: &[u8]) -> usize {
+    unsafe { avxcount(buffer.as_ptr(), buffer.len()) }
+}
+
+pub fn newlinecount_avxuu(buffer: &[u8]) -> usize {
+    unsafe { avxcountuu(buffer.as_ptr(), buffer.len()) }
+}
+
+pub fn newlinecount_avxu(buffer: &[u8]) -> usize {
+    unsafe { avxcountu(buffer.as_ptr(), buffer.len()) }
+}
+
 #[cfg(test)]
 extern crate quickcheck;
 #[cfg(test)]
@@ -19,7 +52,7 @@ mod tests {
 
     #[test]
     fn quickcheck() {
-        let n = 100_000;
+        let n = 1_000_000;
         QuickCheck::new()
             .tests(n)
             .max_tests(n * 100)
